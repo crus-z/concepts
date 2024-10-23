71b4c3a4892b7a37091df14db29ca5b0d860bb8c
crus-z <cristianomelodf@gmail.com>
2024-10-22 22:58:19 -0300

    commit

---
+++ stringlength.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string enter;
+	cout << "Write a string to find its length:" << endl;
+	cin >> enter;
+	
+
+	cout << "The length of " << enter << " is " << enter.length() << endl;
+}
